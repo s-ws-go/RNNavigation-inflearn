1d1784ede641ef62c7b6ccaddb3df9ee6925b289
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-25 21:44:38 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello world!!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
